066df9d97316c6a137197ab1a24e61652d21b887
JP <juhapekka.arimaa@gmail.com>
2019-02-17 20:26:51 +0200

    Add glfw and glad, create window

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,39 @@
 #include <stb_image_write.h>
 #include <glm/glm.hpp>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-    glm::vec3 v(1.0f, 2.0f, 3.0f);
-    std::cout << v.x << " " << v.y << " " << v.z << "\n";
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1600, 900, "GL", NULL, NULL);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD\n";
+        return 2;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
